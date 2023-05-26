f404140ddd64c816a76490167203bf9f2be0fe1b
Octavio Lara <octavioasapchi@gmail.com>
2023-05-26 17:29:26 -0400

    [:wrench: TRY] freezing requirements.txt

--- render_build.sh
+++ render_build.sh
@@ -1,10 +1,2 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# npm install
-# npm run build
-
-pipenv install
-
-pipenv run upgrade
+python3 install -r requirements.txt
+python3 flask db upgrade

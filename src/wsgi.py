55905887ae2d9d21a7e6ebd2bd9df6d0b82bc520
Octavio Lara <octavioasapchi@gmail.com>
2023-05-29 02:46:02 -0400

    render deploy

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,5 +1,7 @@
 from src.Server.app import create_app, db
+from src.Server.app import create_app
 
-if __name__ == "__main__":
-    app = create_app()
+app = create_app()
+
+if __name__ == '__main__':
     app.run()
